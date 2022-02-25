e6af729dc07d21491da65eb5cdb6dbb7b32b8df4
juliodive <juliodive@gmail.com>
2022-02-25 09:42:41 -0400

    Refactoring Address

--- LoremIpsumLogistica.Api/Domain/Entities/Address.cs
+++ LoremIpsumLogistica.Api/Domain/Entities/Address.cs
@@ -1,12 +1,12 @@
-﻿using LoremIpsumLogistica.Api.Domain.Enums;
+﻿using LoremIpsumLogistica.Api.Domain.Entities.Base;
+using LoremIpsumLogistica.Api.Domain.Enums;
 using LoremIpsumLogistica.Api.Domain.Extensions.Validations;
 using System.ComponentModel.DataAnnotations;
 
 namespace LoremIpsumLogistica.Api.Domain.Entities
 {
-    public class Address
+    public class Address : EntityBase
     {
-        public Guid Id { get; private set; }
         public TypeAddress TypeAddress { get; private set; }
         public int ZipCode { get; private set; }
         public string Place { get; private set; }
@@ -14,9 +14,22 @@ namespace LoremIpsumLogistica.Api.Domain.Entities
         public string Complement { get; private set; }
         public string District { get; private set; }
         public string State { get; private set; }
+        public Guid ClientId { get; set; }
+        public Client Client { get; set; }
         private Address()
         {}
-        public Address(TypeAddress typeAddress, int zipCode, string place, int number, string complement, string district, string state)
+        public Address(Guid clientId,TypeAddress typeAddress, int zipCode, string place, int number, string complement, string district, string state)
+        {
+            ClientId = clientId;
+            TypeAddress = typeAddress;
+            ZipCode = ZipCodeValidator(zipCode);
+            Place = StringValidator.Validating("Place", place, 5, 150);
+            Number = NumberValidator(number);
+            Complement = complement;
+            District = district;
+            State = StateValidator.Validating(state);
+        }
+        public void Update(TypeAddress typeAddress, int zipCode, string place, int number, string complement, string district, string state)
         {
             TypeAddress = typeAddress;
             ZipCode = ZipCodeValidator(zipCode);
