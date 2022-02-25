1376dcf6358272591c363664fedbb9a21b7c8d9b
juliodive <juliodive@gmail.com>
2022-02-25 09:59:55 -0400

    Create Service Address

---
+++ LoremIpsumLogistica.Api/Domain/IServices/IAddressService.cs
@@ -0,0 +1,12 @@
+﻿namespace LoremIpsumLogistica.Api.Domain.IServices
+{
+    public interface IAddressService
+    {
+        Task<AddressResponse> Create(CreateAddressRequest request);
+        Task<IEnumerable<AddressResponse>> GetAll(int page, int size);
+        Task<AddressResponse> GetById(Guid id);
+        Task<IEnumerable<AddressResponse>> GetByClient(Guid clientId);
+        Task<AddressResponse> Update(Guid id, UpdateAddressRequest request);
+        Task DeleteByI(Guid id);
+    }
+}
---
+++ LoremIpsumLogistica.Api/Domain/IServices/Services/AddressService.cs
@@ -0,0 +1,31 @@
+﻿namespace LoremIpsumLogistica.Api.Domain.IServices.Services
+{
+    public class AddressService : IAddressService
+    {
+        public Task<AddressResponse> Create(CreateAddressRequest request)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IEnumerable<AddressResponse>> GetAll(int page, int size)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<AddressResponse> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IEnumerable<AddressResponse>> GetByClient(Guid clientId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<AddressResponse> Update(Guid id, UpdateAddressRequest request)
+        {
+            throw new NotImplementedException();
+        }
+        public Task DeleteByI(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
