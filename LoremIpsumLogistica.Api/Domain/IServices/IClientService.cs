87d9667a30c7be34b9b88d0c754956e119ed5b15
juliodive <juliodive@gmail.com>
2022-02-25 09:55:55 -0400

    Create Service Client

---
+++ LoremIpsumLogistica.Api/Domain/IServices/IClientService.cs
@@ -0,0 +1,12 @@
+﻿namespace LoremIpsumLogistica.Api.Domain.IServices
+{
+    public interface IClientService
+    {
+        Task<ClientResponse> Create(CreateClientRequest request);
+        Task<IEnumerable<ClientResponse>> GetAll(int page, int size);
+        Task<ClientResponse> GetById(Guid id);
+        Task<IEnumerable<ClientResponse>> GetByName(int page, int size,string firstName, string lastName);
+        Task<ClientResponse> Update(Guid id, UpdateClientRequest request);
+        Task DeleteByI(Guid id);
+    }
+}
---
+++ LoremIpsumLogistica.Api/Domain/IServices/Services/ClientService.cs
@@ -0,0 +1,34 @@
+﻿namespace LoremIpsumLogistica.Api.Domain.IServices.Services
+{
+    public class ClientService : IClientService
+    {
+        public Task<ClientResponse> Create(CreateClientRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ClientResponse>> GetAll(int page, int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ClientResponse> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ClientResponse>> GetByName(int page, int size, string firstName, string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ClientResponse> Update(Guid id, UpdateClientRequest request)
+        {
+            throw new NotImplementedException();
+        }
+        public Task DeleteByI(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
