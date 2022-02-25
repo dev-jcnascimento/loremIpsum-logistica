41c183ec948f0f4e7eacbfe4a729e8a4ac83f92b
juliodive <juliodive@gmail.com>
2022-02-25 09:37:09 -0400

    Refactoring Client

--- LoremIpsumLogistica.Api/Domain/Entities/Client.cs
+++ LoremIpsumLogistica.Api/Domain/Entities/Client.cs
@@ -1,20 +1,28 @@
-﻿using LoremIpsumLogistica.Api.Domain.Enums;
+﻿using LoremIpsumLogistica.Api.Domain.Entities.Base;
+using LoremIpsumLogistica.Api.Domain.Enums;
 using LoremIpsumLogistica.Api.Domain.Extensions.Validations;
 
 namespace LoremIpsumLogistica.Api.Domain.Entities
 {
-    public class Client
+    public class Client : EntityBase
     {
-        public Guid Id { get; private set; }
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
         public DateTime BirthDate { get; private set; }
         public Genre Genre { get; private set; }
+        public ICollection<Address> Addresses { get; private set; }
         private Client()
         {}
         public Client(string firstName, string lastName, DateTime birthDate, Genre genre)
         {
             FirstName = StringValidator.Validating("FirstName", firstName,3,60);
+            LastName = StringValidator.Validating("LastName", lastName, 3, 60);
+            BirthDate = birthDate;
+            Genre = genre;
+        }
+        public void Update(string firstName, string lastName, DateTime birthDate, Genre genre)
+        {
+            FirstName = StringValidator.Validating("FirstName", firstName, 3, 60);
             LastName = StringValidator.Validating("LastName", lastName, 3, 60);
             BirthDate = birthDate;
             Genre = genre;
