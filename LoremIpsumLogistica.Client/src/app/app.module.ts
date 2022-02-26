07106caa0c9e8ee8b2b627f8c48ebb245b92d4a8
juliodive <juliodive@gmail.com>
2022-02-26 10:42:23 -0400

    Create Components Client and Address

--- LoremIpsumLogistica.Client/src/app/app.module.ts
+++ LoremIpsumLogistica.Client/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { ClientsComponent } from './clients/clients.component';
+import { AddressesComponent } from './addresses/addresses.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientsComponent,
+    AddressesComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
